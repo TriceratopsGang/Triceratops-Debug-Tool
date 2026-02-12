2a711960de70f102971708e262fca651d6085af1
Andrew <91447105+TriceratopsGang@users.noreply.github.com>
2026-02-12 12:57:21 -0500

    Many changes
    - Fixed admin check
    - Fixed hangs by removing "systeminfo"
    - Changed all func names (RIP ass func)
    - Removed deprecated "WmiObject "
    - Removed unused "setlocal"
    - Changed title headers from "=" to "-"
    - Removed redundant exit func
    - Adjusted comments

--- TriceratopsDebugTool.bat
+++ TriceratopsDebugTool.bat
@@ -1,21 +1,23 @@
 @echo off
+cd /d "%~dp0"
 title Triceratops Debug Tool
 color 0E
-setlocal enabledelayedexpansion
 
-:: Check if running as administrator
-net session >nul 2>&1
+:: Admin check
+fltmc >nul 2>&1
 if %errorlevel% neq 0 (
     echo Requesting administrative privileges . . .
     powershell -Command "Start-Process '%~f0' -Verb runAs"
     exit /b
 )
 
+:: = = = = = = = = = = = = = = = = = = = =
+:: Main Menu
 :mainMenu
 cls
-echo ==================================================
+echo - - - - - - - - - - - - - - - - - - - -
 echo Main Menu
-echo ==================================================
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo 1. System File Checker
 echo 2. Deployment Image Servicing and Management
@@ -31,31 +33,34 @@ echo.
 
 set /p choice=Select an option: 
 
-if "%choice%"=="1" call :sfc
-if "%choice%"=="2" call :dism
-if "%choice%"=="3" call :rwe
-if "%choice%"=="4" call :dod
-if "%choice%"=="5" call :odc
-if "%choice%"=="6" call :scp
-if "%choice%"=="7" call :ass
-if "%choice%"=="8" call :lsi
+if "%choice%"=="1" goto :systemFileChecker
+if "%choice%"=="2" goto :deploymentImageServicingManagement
+if "%choice%"=="3" goto :restartWindowsExplorer
+if "%choice%"=="4" goto :defragmentDrives
+if "%choice%"=="5" goto :diskCleanup
+if "%choice%"=="6" goto :soundControlPanel
+if "%choice%"=="7" goto :advancedSystemSettings
+if "%choice%"=="8" goto :systemInformation
 if "%choice%"=="9" goto :eof
 
 cls
-echo Invalid option selected.
+echo Invalid option selected
 pause
 goto :mainMenu
 
-:: ---- System File Checker ----
-:sfc
+:: = = = = = = = = = = = = = = = = = = = =
+:: System File Checker
+:systemFileChecker
 cls
-echo ==================================================
+echo - - - - - - - - - - - - - - - - - - - -
 echo System File Checker
-echo ==================================================
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
-echo 1. Scan Now    // Scans all protected system files and attempts to repair any issues found.
-echo 2. Verify Only // Scans all protected system files, but does not attempt to repair them.
-==
+echo Please Select an Option . . .
+echo.
+echo 1. Scan Now                // Scans all protected system files and attempts to repair any issues found
+echo 2. Verify Only             // Scans all protected system files, but does not attempt to repair them
+echo.
 echo 3. Return to Main Menu
 echo.
 
@@ -66,7 +71,7 @@ if "%choice%"=="2" call :verifyOnly
 if "%choice%"=="3" goto :mainMenu
 
 cls
-echo Invalid option selected.
+echo Invalid option selected
 pause
 goto :mainMenu
 
@@ -86,16 +91,19 @@ echo.
 pause
 goto :mainMenu
 
-:: ---- Deployment Image Servicing and Management ----
-:dism
+:: = = = = = = = = = = = = = = = = = = = =
+:: Deployment Image Servicing and Management
+:deploymentImageServicingManagement
 cls
-echo ==================================================
+echo - - - - - - - - - - - - - - - - - - - -
 echo Deployment Image Servicing and Management
-echo ==================================================
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
-echo 1. Check Health    // Performs a quick check to determine if the Windows image has been corrupted.
-echo 2. Scan Health     // Executes a more comprehensive scan for image corruption.
-echo 3. Restore Health  // Scans for and repairs corrupted files in the Windows image.
+echo Please Select an Option . . .
+echo.
+echo 1. Check Health            // Performs a quick check to determine if the Windows image has been corrupted
+echo 2. Scan Health             // Executes a more comprehensive scan for image corruption
+echo 3. Restore Health          // Scans for and repairs corrupted files in the Windows image
 echo.
 echo 4. Return to Main Menu
 echo.
@@ -108,7 +116,7 @@ if "%choice%"=="3" call :restoreHealth
 if "%choice%"=="4" goto :mainMenu
 
 cls
-echo Invalid option selected.
+echo Invalid option selected
 pause
 goto :mainMenu
 
@@ -136,136 +144,177 @@ echo.
 pause
 goto :mainMenu
 
-:: ---- Restarting Windows Explorer ----
-:rwe
+:: = = = = = = = = = = = = = = = = = = = =
+:: Restarting Windows Explorer
+:restartWindowsExplorer
 cls
-echo Restarting Windows Explorer . . .
-taskkill /f /im explorer.exe
-timeout /t 1 /nobreak > nul
-start explorer.exe
+echo - - - - - - - - - - - - - - - - - - - -
+echo Restart Windows Explorer
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
+echo This might take awhile . . .
+echo.
+taskkill /f /im explorer.exe >nul 2>&1
+call :wait 1
+tasklist /fi "imagename eq explorer.exe" | find /i "explorer.exe" >nul
+if errorlevel 1 (start "" explorer.exe)
 pause
-goto :mainMenu
+goto :mainMenu  
 
-:: ---- Defragment and Optimize Drives ----
-:dod
+:: = = = = = = = = = = = = = = = = = = = =
+:: Defragment and Optimize Drives
+:defragmentDrives
 cls
-echo Defragment and Optimize Drives . . .
+echo - - - - - - - - - - - - - - - - - - - -
+echo Defragment and Optimize Drives
+echo - - - - - - - - - - - - - - - - - - - -
+echo.
+echo Drive Optimizer . . .
+call :wait 1
 start dfrgui.exe
 echo.
 pause
 goto :mainMenu
 
-:: ---- Open Disk Cleanup ----
-:odc
+:: = = = = = = = = = = = = = = = = = = = =
+:: Open Disk Cleanup
+:diskCleanup
 cls
+echo - - - - - - - - - - - - - - - - - - - -
+echo Disk Cleanup
+echo - - - - - - - - - - - - - - - - - - - -
+echo.
 echo Opening Disk Cleanup . . .
+call :wait 1
 start cleanmgr.exe
 echo.
 pause
 goto :mainMenu
 
-:: ---- Open Sound Control Panel ----
-:scp
+:: = = = = = = = = = = = = = = = = = = = =
+:: Open Sound Control Panel
+:soundControlPanel
 cls
+echo - - - - - - - - - - - - - - - - - - - -
+echo Sound Control Panel
+echo - - - - - - - - - - - - - - - - - - - -
+echo.
 echo Opening Sound Control Panel . . .
+call :wait 1
 control mmsys.cpl sounds
 echo.
 pause
 goto :mainMenu
 
-:: ---- Open Advanced System Settings ----
-:ass
+:: = = = = = = = = = = = = = = = = = = = =
+:: Open Advanced System Settings
+:advancedSystemSettings
 cls
+echo - - - - - - - - - - - - - - - - - - - -
+echo Advanced System Settings
+echo - - - - - - - - - - - - - - - - - - - -
+echo.
 echo Opening Advanced System Settings . . .
+call :wait 1
 sysdm.cpl
 echo.
 pause
 goto :mainMenu
 
-:: ---- List System Information ----
-:lsi
+:: = = = = = = = = = = = = = = = = = = = =
+:: List System Information
+:systemInformation
 cls
-echo ==================================================
+echo - - - - - - - - - - - - - - - - - - - -
 echo System Information
-echo ==================================================
+echo - - - - - - - - - - - - - - - - - - - -
+echo.
+echo This might take awhile . . .
 echo.
 echo OS Information:
-systeminfo | findstr /B /C:"OS"
+for /f "tokens=2,*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| find "ProductName"') do set OSName=%%B
+for /f "tokens=2,*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v DisplayVersion ^| find "DisplayVersion"') do set OSVer=%%B
+for /f "tokens=2,*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentBuild ^| find "CurrentBuild"') do set OSBuild=%%B
+echo %OSName% %OSVer% (Build %OSBuild%)
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo BIOS Information:
-powershell -Command "Get-WmiObject -Class Win32_BIOS | Select-Object Manufacturer, Name, Version"
+powershell -Command "Get-CimInstance Win32_BIOS | Select-Object Manufacturer, Name, Version"
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo Motherboard Information:
 powershell -Command "Get-WmiObject -Class Win32_BaseBoard | Select-Object Manufacturer, Product, SerialNumber"
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo System Architecture: %PROCESSOR_ARCHITECTURE%
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo CPU Information:
 powershell -Command "Get-WmiObject -Class Win32_Processor | Select-Object -ExpandProperty Name"
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo RAM Information:
-systeminfo | findstr /C:"Total Physical Memory"
+for /f "tokens=2 delims==" %%A in ('powershell -NoLogo -Command "(Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory"') do set RAM=%%A
+for /f %%A in ('powershell -NoLogo -Command "[math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1GB,2)"') do set RAMGB=%%A
+echo Total Physical Memory: %RAMGB% GB
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo GPU Information:
 powershell -Command "Get-WmiObject -Class Win32_VideoController | Select-Object Name, AdapterRAM, DriverVersion"
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo Storage Information:
 powershell -Command "Get-WmiObject -Class Win32_LogicalDisk | Select-Object DeviceID, @{Name='FreeSpace(GB)';Expression={[math]::round($_.FreeSpace/1GB,2)}}, @{Name='Size(GB)';Expression={[math]::round($_.Size/1GB,2)}}"
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 ipconfig
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 getmac
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
-systeminfo | findstr /C:"System Boot Time"
+for /f %%A in ('powershell -NoLogo -Command "(Get-CimInstance Win32_OperatingSystem).LastBootUpTime.ToLocalTime().ToString(\"yyyy-MM-dd HH:mm:ss\")"') do set BootTime=%%A
+echo System Boot Time: %BootTime%
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo USB Devices:
 powershell -Command "Get-WmiObject -Class Win32_USBHub | Select-Object DeviceID, PNPDeviceID, Description"
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo Sound Devices:
 powershell -Command "Get-WmiObject -Class Win32_SoundDevice | Select-Object Name, Manufacturer, Status"
 echo.
-echo --------------------------------------------------
-echo.
-echo Running Processes:
-powershell -Command "Get-Process | Select-Object Name, Id, CPU, StartTime"
-echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 echo Installed Programs:
-powershell -Command "Get-WmiObject -Class Win32_Product | Select-Object Name, Version"
+powershell -Command "Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion"
 echo.
-echo --------------------------------------------------
+echo - - - - - - - - - - - - - - - - - - - -
 echo.
 pause
 goto :mainMenu
 
-goto :eof
+:: = = = = = = = = = = = = = = = = = = = =
+:: Shared
+:wait
+set "_w=%~1"
+if "%_w%"=="" set "_w=1"
+set /a _w=_w+1
+ping 127.0.0.1 -n %_w% >nul
+exit /b
 
-:eof
-cls
-exit
+:: = = = = = = = = = = = = = = = = = = = =
+:: Exit
+goto :eof
